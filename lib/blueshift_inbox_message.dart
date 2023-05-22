e3daac8c11b199172f53dc4e2d418cf393c0ed5e
Ketan Shikhare <ketan.shikhare@getblueshift.com>
2023-05-22 12:35:44 +0530

    updated the message model

--- lib/blueshift_inbox_message.dart
+++ lib/blueshift_inbox_message.dart
@@ -1,35 +1,33 @@
 class BlueshiftInboxMessage {
   final int? id; // ID in local DB
   final String? messageId;
-  final String? displayOn;
-  final String? trigger;
-  final String? messageType;
-  final String? scope;
+  final String? title;
+  final String? detail;
+  final String? imageUrl;
+  String? status;
   final Map? data;
   final DateTime? createdAt;
-  String? status;
 
-  BlueshiftInboxMessage(
-      {this.id,
-      this.messageId,
-      this.displayOn,
-      this.trigger,
-      this.messageType,
-      this.scope,
-      this.status,
-      this.data,
-      this.createdAt});
+  BlueshiftInboxMessage({
+    this.id,
+    this.messageId,
+    this.title,
+    this.detail,
+    this.imageUrl,
+    this.status,
+    this.data,
+    this.createdAt,
+  });
 
   BlueshiftInboxMessage.fromJson(Map data)
       : this(
           id: data["id"],
           messageId: data["messageId"],
-          displayOn: data["displayOn"],
-          trigger: data["trigger"],
-          messageType: data["messageType"],
-          scope: data["scope"],
-          data: data["data"],
+          title: data["title"],
+          detail: data["detail"],
+          imageUrl: data["imageUrl"],
           status: data["status"],
+          data: data["data"],
           createdAt: DateTime.fromMicrosecondsSinceEpoch(
               (data["createdAt"] as int) * 1000000),
         );
@@ -41,10 +39,9 @@ class BlueshiftInboxMessage {
     return {
       'id': id,
       'messageId': messageId,
-      'displayOn': displayOn,
-      'trigger': trigger,
-      'messageType': messageType,
-      'scope': scope,
+      'title': title,
+      'detail': detail,
+      'imageUrl': imageUrl,
       'data': data,
       'status': status,
       'createdAt': sec,
